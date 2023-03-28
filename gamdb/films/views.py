57e859d74a4992d2b345b956ac6b87a0fbfa6e0a
FilipekBenes <83021112+FilipekBenes@users.noreply.github.com>
2023-03-28 19:43:59 +0200

    first html

--- gamdb/films/views.py
+++ gamdb/films/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+def homepage(request):
+    context = {
+        "movie": Movie.objects.all(),
+    }
+    return render (request, 'main.html', context)
+
